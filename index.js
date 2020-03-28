c0b3457be8bc941ffce4ffee50557a95fb4ecae8
Iván Alonso <ivan@neverbot.com>
2020-03-28 18:51:14 +0100

    feat(helpers): url_for working

--- index.js
+++ index.js
@@ -7,13 +7,17 @@ if (
 ) {
   hexo.log.info(
     'Plugins config not found in _config.yml.\n' +
-      'Visit https://github.com/neverbot/hexo-multilang for more information.'
+      '      Visit https://github.com/neverbot/hexo-multilang for more information.'
   );
   return;
 }
 
 // Helpers
 var helpers = require('./lib/helpers');
+
+// mask the original url_for
+const original_url_for = hexo.extend.helper.get('url_for').bind(hexo);
+hexo.extend.helper.register('__hexo_multilang_url_for', original_url_for);
 
 hexo.extend.helper.register('get_languages', helpers.get_languages);
 hexo.extend.helper.register('url_for', helpers.url_for);
